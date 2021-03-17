9cdf1b6e5f191b69e059ca82ba03b1fe6669a7ea
Dima <dmitry.turov374@gmail.com>
2021-03-17 13:40:23 +0300

    lesson 19

--- src/main/java/com/example/controllers/FirsController.java
+++ src/main/java/com/example/controllers/FirsController.java
@@ -2,13 +2,38 @@ package com.example.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 //@RequestMapping("/first")
 public class FirsController {
 
+    /*
+     * Если в мктод GET не передать параметры
+     * HttpServletRequest request будет равен null
+     */
+
+//    @GetMapping("/hello")
+//    public String helloPage(HttpServletRequest request) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//
+//        System.out.println("Hello, " + name + " " + surname);
+//        return "first/hello";
+//    }
+
+    /**
+     * Если в мктод GET не передать параметры
+     * возникнет ошибка
+     * чтобы этого избежать добавляем required = false
+     */
+
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
